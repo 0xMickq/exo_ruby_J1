afd0655c9068d152576c0f48518f455f73d0be18
0xMickq <mickqdev@gmail.com>
2025-03-10 16:19:23 +0100

    adding the fifth exercice

---
+++ exo_05.rb
@@ -0,0 +1,38 @@
+#Affiche la phrase "On va compter le nombre d'heures de travail à THP"
+puts "On va compter le nombre d'heures de travail à THP"
+#Affiche la phrase "Travail : 550" car le programme fait le calcul de ce qu'il y'a entre #{}
+puts "Travail : #{10 * 5 * 11}"
+#Affiche la phrase "En minutes ça fait : 33000" car le programme fait le calcul de ce qu'il y'a entre #{}
+puts "En minutes ça fait : #{10 * 5 * 11 * 60}"
+
+#Affiche la phrase "Et en secondes ?"
+puts "Et en secondes ?"
+
+#affiche 1980000 car le programme fait le calcul
+puts 10 * 5 * 11 * 60 * 60
+
+#affiche la phrase "Est-ce que c'est vrai que 3 + 2 < 5 - 7 ?"
+puts "Est-ce que c'est vrai que 3 + 2 < 5 - 7 ?"
+
+#affiche "false" car le programme va calculer 3+2=5 puis 5-7=-2 et va trouver que -2 n'est pas supérieur à 5
+puts 3 + 2 < 5 - 7
+
+#affiche la phrase "Ça fait combien 3 + 2 ? 5" car le programme fait le calcul de ce qu'il y'a entre #{}
+puts "Ça fait combien 3 + 2 ? #{3 + 2}"
+#affiche la phrase "Ça fait combien 5 - 7 ? -2" car le programme fait le calcul de ce qu'il y'a entre #{}
+puts "Ça fait combien 5 - 7 ? #{5 - 7}"
+
+#affiche "Ok, c'est faux alors !"
+puts "Ok, c'est faux alors !"
+
+#affiche "C'est drôle ça, faisons-en plus :"
+puts "C'est drôle ça, faisons-en plus :"
+
+#affiche "Est-ce que 5 est plus grand que -2 ? true" car le programme trouve que 5 est bien supérieur à -2
+puts "Est-ce que 5 est plus grand que -2 ? #{5 > -2}"
+#affiche "Est-ce que 5 est supérieur ou égal à -2 ? true" car le programme trouve que 5 est bien supérieur ou égal à -2
+puts "Est-ce que 5 est supérieur ou égal à -2 ? #{5 >= -2}"
+#affiche "Est-ce que 5 est inférieur ou égal à -2 ? false" car le programme trouve que -2 est bien inférieur ou égal à 5
+puts "Est-ce que 5 est inférieur ou égal à -2 ? #{5 <= -2}"
+
+# #{} sert à appeler une variable dans un string
