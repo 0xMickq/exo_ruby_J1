7f5af9b0ea792f1a407dc374117edcdeb84e188a
0xMickq <mickqdev@gmail.com>
2025-03-10 16:32:24 +0100

    adding the seventh exercice

---
+++ exo_07_a.rb
@@ -0,0 +1,5 @@
+puts "Bonjour, c'est quoi ton blase ?"
+user_name = gets.chomp
+puts user_name
+
+#gets.chomp à l'air de permettre à l'utilisateur de rentrer des caractères dans un champ, et une fois validée le programme renvoie la réponse de l'utilisateur 
