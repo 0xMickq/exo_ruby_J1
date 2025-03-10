250479c0086d4dbe07e418f05d246b6b41d84e5f
0xMickq <mickqdev@gmail.com>
2025-03-10 16:44:09 +0100

    adding the ninth exercice

---
+++ exo_09.rb
@@ -0,0 +1,7 @@
+puts "Quel est votre prénom ?"
+print ">"
+user_name = gets.chomp
+puts "Quel est votre nom ?"
+print ">"
+user_last_name = gets.chomp
+puts "Bonjour #{user_name} #{user_last_name} !"
