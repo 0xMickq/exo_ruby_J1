37b257fea700304a64dc4461f9edebb5077ede83
0xMickq <mickqdev@gmail.com>
2025-03-10 16:05:47 +0100

    adding the 3rd and the fourth exercice

---
+++ exo_03.rb
@@ -0,0 +1,4 @@
+puts "Hello, world !"
+#puts "Et avec une voix sexy, ça donne : Hello, world !"
+
+#La secondes commande "puts" ne se lance pas car il y'a un # devant, le # sert à mettre un commentaire dans le code
---
+++ exo_04.rb
@@ -0,0 +1,3 @@
+puts "Salut, ça farte ?"
+
+#Si on ne met pas les guillemets de fin le code ne marche pas parce que le texte à l'interieur des guillemets ne peut pas être lu quand il en manque
